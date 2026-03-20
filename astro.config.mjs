8462e45ccea179536ce01944bbb85de366f24478
Kian <81150384+cryling@users.noreply.github.com>
2026-03-20 13:15:24 +0100

    fix data fetching

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,17 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import solidJs from "@astrojs/solid-js";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs()],
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: node({
+    mode: "standalone"
+  })
 });
