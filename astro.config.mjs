d038fb3a9b98b3bf2bb08c0d66c4ff184481a91e
Kian <81150384+cryling@users.noreply.github.com>
2026-03-20 17:04:49 +0100

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import solidJs from "@astrojs/solid-js";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://genderengine.kianreiling.com',
-  integrations: [solidJs()],
+  integrations: [solidJs(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()]
